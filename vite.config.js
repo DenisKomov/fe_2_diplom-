dd2fa0f26e6e6248555107853c3b6e4df0a0f9ad
DenisKomov <143321805+DenisKomov@users.noreply.github.com>
2025-08-09 15:12:40 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    root: './fe_2_diplom-',
+    root: './diplom',
     plugins: [react()],
 })
